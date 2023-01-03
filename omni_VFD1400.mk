b3efee014e583a4da34eed31702cecbbaf3c0b44
gella27121992 <118847446+gella27121992@users.noreply.github.com>
2023-01-03 18:02:32 +0100

    Add files via upload

---
+++ omni_VFD1400.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from VFD1400 device
+$(call inherit-product, device/vodafone/VFD1400/device.mk)
+
+PRODUCT_DEVICE := VFD1400
+PRODUCT_NAME := omni_VFD1400
+PRODUCT_BRAND := Vodafone
+PRODUCT_MODEL := VFD 1400
+PRODUCT_MANUFACTURER := vodafone
+
+PRODUCT_GMS_CLIENTID_BASE := android-alcatel
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="pop4104gvdf-user 6.0.1 MMB29M vBEQ-0 release-keys"
+
+BUILD_FINGERPRINT := Vodafone/Tab_prime7/VFD1400:6.0.1/MMB29M/vBEQ-0:user/release-keys
